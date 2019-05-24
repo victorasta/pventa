4693c5e287f01741ce56255156391106f1aff951
victor <victor589c@gmail.com>
2019-05-24 08:04:45 -0600

    Primer commit

---
+++ app/language/spanish/auth_lang.php
@@ -0,0 +1,94 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+ * Module: Auth
+ * Language: English
+ * 
+ * Last edited:
+ * 30th April 2015
+ *
+ * Package:
+ * Simple POS - Point of sale made easy
+ * 
+ * You can translate this file to your language. 
+ * For instruction on new language setup, please visit the documentations. 
+ * You also can share your language files by emailing to saleem@tecdiary.com 
+ * Thank you 
+ */
+
+$lang['sign_in']                                    ="Registrarse";
+$lang['notify_user_by_email']                       ="Notificar al usuario por correo electrónico";
+$lang['group']                                      ="Grupo";
+$lang['edit_user']                                  ="Editar usuario";
+$lang['delete_user']                                ="Borrar usuario";
+$lang['user_added']                                 ="Usuario agregado exitosamente";
+$lang['user_updated']                               ="Usuario actualizado correctamente";
+$lang['users_deleted']                              ="Usuarios eliminados con éxito";
+$lang['alert_x_user']                               ="Vas a eliminar a este usuario de forma permanente. Presiona OK para continuar y Cancelar para regresar";
+$lang['login_email']                                ="Ingreso (correo";
+$lang['edit_profile']                               ="Editar perfil";
+$lang['website']                                    ="Sitio web";
+$lang['if_you_need_to_rest_password_for_user']      ="Si necesita restablecer la contraseña para este usuario.";
+$lang['user_options']                               ="Opciones de usuario";
+$lang['old_password']                               ="Contraseña anterior";
+$lang['new_password']                               ="Nueva contraseña";
+$lang['change_avatar']                              ="Cambiar avatar";
+$lang['update_avatar']                              ="Actualizar Avatar";
+$lang['avatar']                                     ="Avatar";
+$lang['avatar_deleted']                             ="Avatar actualizado con éxito";
+$lang['captcha_wrong']                              ="El Captcha está equivocado o ha caducado. Inténtalo de nuevo";
+$lang['captcha']                                    ="Captcha";
+$lang['site_is_offline_plz_try_later']              ="El sitio está desconectado. Visítenos nuevamente en unos días";
+$lang['type_captcha']                               ="Tipo Captcha";
+$lang['we_are_sorry_as_this_sction_is_still_under_development'] ="Lo sentimos, ya que esta sección aún está en desarrollo y estamos tratando de hacerlo lo antes posible. Su paciente es muy apreciado";
+$lang['confirm']                                    ="Confirmación";
+$lang['error_csrf']                                 ="Se detectó una falsificación de solicitud entre sitios o el token de csrf ha caducado. Inténtelo de nuevo";
+$lang['avatar_updated']                             ="Avatar actualizado con éxito";
+$lang['registration_is_disabled']                   ="El registro de la cuenta está cerrado.";
+$lang['login_to_your_account']                      ="Por favor, ingrese a su cuenta.";
+$lang['pw']                                         ="Contraseña";
+$lang['remember_me']                                ="Recuérdame";
+$lang['forgot_your_password']                       ="¿Olvidaste tu contraseña?";
+$lang['dont_worry']                                 ="¡No te preocupes!";
+$lang['click_here']                                 ="haga clic aquí";
+$lang['to_rest']                                    ="reiniciar";
+$lang['forgot_password']                            ="¿Se te olvidó tu contraseña?";
+$lang['login_successful']                           ="Has iniciado sesión correctamente.";
+$lang['back']                                       ="Espalda";
+$lang['dont_have_account']                          ="¿No tienes cuenta?";
+$lang['no_worry']                                   ="¡Sin preocupaciones!";
+$lang['to_register']                                ="registrarse";
+$lang['register_account_heading']                   ="Por favor, rellene el siguiente formulario para registrar la cuenta";
+$lang['register_now']                               ="Regístrate ahora";
+$lang['no_user_selected']                           ="Ningún usuario seleccionado. Seleccione al menos un usuario.";
+$lang['delete_users']                               ="Eliminar usuarios";
+$lang['delete_avatar']                              ="Eliminar Avatar";
+$lang['deactivate_heading']                         ="¿Está seguro de desactivar al usuario?";
+$lang['deactivate']                                 ="Desactivar";
+$lang['pasword_hint']                               ="Al menos 1 mayúscula, 1 minúscula, 1 número y más de 8 caracteres de longitud";
+$lang['pw_not_same']                                ="La contraseña y la contraseña de confirmación no son las mismas";
+$lang['reset_password']                             ="Restablecer la contraseña";
+$lang['reset_password_link_alt']                    ="Puede pegar este código a continuación en su url si el enlace anterior no funciona";
+$lang['email_forgotten_password_subject']           ="Restablecer detalles de contraseña";
+$lang['reset_password_email']                       ="Restablecer contraseña% s";
+$lang['back_to_login']                              ="Atrás para iniciar sesión";
+$lang['forgot_password_unsuccessful']               ="Error al restablecer la contraseña";
+$lang['forgot_password_successful']                 ="La contraseña se restableció con éxito, use la nueva contraseña para iniciar sesión";
+$lang['password_change_unsuccessful']               ="Cambio de contraseña falló";
+$lang['password_change_successful']                 ="Contraseña cambiada correctamente";
+$lang['forgot_password_email_not_found']            ="La dirección de correo electrónico enter no pertenece a ninguna cuenta.";
+$lang['login_unsuccessful']                         ="Acceso fallido. Por favor intente nuevamente";
+$lang['email_forgot_password_link']                 ="Restablecer enlace de contraseña";
+$lang['reset_password_heading']                     ="Restablecer la contraseña";
+$lang['reset_password_new_password_label']          ="Nueva contraseña";
+$lang['reset_password_new_password_confirm_label']  ="Confirmar nueva contraseña";
+$lang['register']                                   ="Registro";
+$lang['reset_password_submit_btn']                  ="Restablecer la contraseña";
+$lang['error_csrf']                                 ='EL formulario no pasa las medidas de seguridad';
+$lang['account_creation_successful']                ="Cuenta creada con éxito";
+$lang['old_password_wrong']                         ="Por favor, escriba la contraseña antigua correcta";
+$lang['sending_email_failed']                       ="No se puede enviar el correo electrónico. Si eres administrador, comprueba la configuración; de lo contrario, ponte en contacto con el administrador";
+$lang['deactivate_successful']                      ="Usuario desactivado exitosamente";
+$lang['activate_successful']                        ="Usuario correctamente activado";
+$lang['login_timeout']                              ="Ha fallado 3 intentos de inicio de sesión. Intente después de 10 minutos";
+$lang['forgot_password_heading']                    ="Ingrese su dirección de correo electrónico a continuación para restablecer su contraseña.";
